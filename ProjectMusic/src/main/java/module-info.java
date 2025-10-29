c9117ea644579b6334612b631415fc10e93ecdf0
Kevin3475 <kevine.echeverryv@uqvirtual.edu.co>
2025-10-28 20:37:58 -0500

    buil: base de el proyecto

--- ProjectMusic/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module co.edu.uniquindio.poo.projectmusic {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens co.edu.uniquindio.poo.projectmusic to javafx.fxml;
-    exports co.edu.uniquindio.poo.projectmusic;
-}
