b7547781b4382f9acbc591e69b4b713ee75100db
Kevin3475 <kevine.echeverryv@uqvirtual.edu.co>
2025-10-28 20:39:55 -0500

    Buil: Bases de el proyecto

--- untitled/src/main/java/co/edu/uniquindio/poo/RolParticipante.java
+++
@@ -1,9 +0,0 @@
-package co.edu.uniquindio.poo;
-
-public enum RolParticipante {
-
-    ATLETA,
-    ENTRENADOR,
-    ARBITRO,
-    APOYO
-}
--- untitled/src/main/java/co/edu/uniquindio/poo/TipoEvento.java
+++
@@ -1,8 +0,0 @@
-package co.edu.uniquindio.poo;
-
-public enum TipoEvento {
-
-    COMPETICION,
-    EXHIBICION,
-    TORNEO
-}
